f175d3a46d59ac0ada1015abf6725f50f0947719
Anatoly S <cy25rax@yandex.ru>
2024-01-20 12:21:12 +0500

    seminar3

---
+++ src/main/java/ru/gb/springdemo/api/IssuerController.java
@@ -0,0 +1,38 @@
+package ru.gb.springdemo.api;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.gb.springdemo.model.Issue;
+import ru.gb.springdemo.service.IssuerService;
+
+import java.util.NoSuchElementException;
+
+@Slf4j
+@RestController
+@RequestMapping("/issue")
+public class IssuerController {
+
+  @Autowired
+  private IssuerService service;
+
+  @PostMapping
+  public ResponseEntity<Issue> issueBook(@RequestBody IssueRequest request) {
+    log.info("Получен запрос на выдачу: readerId = {}, bookId = {}", request.getReaderId(), request.getBookId());
+
+    final Issue issue;
+    try {
+      issue = service.issue(request);
+    } catch (NoSuchElementException e) {
+      return ResponseEntity.notFound().build();
+    }
+
+    return ResponseEntity.status(HttpStatus.CONFLICT).body(issue);
+  }
+
+}
